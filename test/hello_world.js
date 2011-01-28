ffb4958c4e37ec7c8c73ca93b545f196c9166ed2
Chris Dew <cmsdew@gmail.com>
2011-01-28 06:42:16 +0000

    package installs and tests are working

---
+++ test/hello_world.js
@@ -0,0 +1,12 @@
+var vows = require('vows')
+  , assert = require('assert')
+  , bdb = require('barricane-prevalent')
+  ;
+
+vows.describe('The First Test').addBatch({
+    'Hello World': { topic: bdb.hello_world
+	               , 'equals "Hello World"': function(topic) {
+						 assert.equal(topic, "Hello World");
+					 }
+				   } 
+}).export(module);
