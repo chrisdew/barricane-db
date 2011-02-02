0c9fa18ed3ce7ba88ac610d9ebc461b441e34521
chrisdew <cmsdew+github@gmail.com>
2011-02-02 13:03:57 +0000

    more changes

--- test/disk-manager.js
+++ test/disk-manager.js
@@ -1,12 +1,21 @@
 var vows = require('vows')
   , assert = require('assert')
   , bdb = require('barricane-db')
+  , fs = require('fs')
   ;
 
 // mock up uuids for testing
 var nextMockUuid = 0;
 function mockUuid() {
 	return "" + nextMockUuid++;
+}
+
+// remove old files, if they exist
+try {
+	fs.unlinkSync('/tmp/testdb.snapshot');
+	fs.unlinkSync('/tmp/testdb.txnlog');
+} catch (e) {
+	console.log("unable to remove files");
 }
 
 vows.describe('Disk Manager Tests').addBatch({
